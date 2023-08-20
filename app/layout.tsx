d3ff62903ad54eeb5ccfab9d3bb91d806f8ad41c
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-08-20 18:06:06 +0530

    url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang="en">
+       <head>
+      <Script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1326277437046150"
+  crossOrigin="anonymous"
+  strategy="lazyOnload"
+/>
+      </head>
       <body className="max-w-screen-2xl mx-auto pt-28">{children}</body>
     </html>
   )
